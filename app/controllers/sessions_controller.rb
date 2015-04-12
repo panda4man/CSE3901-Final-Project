f7b31e77a460c261ae7845c4bb44f20a26b3a0b9
Andrew Clinton <ddrew.clinton@gmail.com>
2015-04-12 15:41:03 -0400

    views model and controller for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+	skip_before_filter :require_login
+	layout "session"
+	def new
+
+	end
+
+	def create
+		user = User.authenticate(params[:email], params[:password])
+		if user.nil?
+			flash.now[:alert] = "Invalid email/password."
+			render :new
+		else
+			sign_in user
+			redirect_to user_url(user.id)
+		end
+	end
+
+	def destroy
+		sign_out
+	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+	def sign_in(user)
+		session[:user_id] = user.id
+		current_user = user
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def sign_out
+		session[:user_id] = nil
+		redirect_to login_path
+	end
+
+	def deny_access
+		redirect_to login_path, :notice => "Please sign in."
+	end
 end
