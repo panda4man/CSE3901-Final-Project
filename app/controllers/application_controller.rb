bdc227716f586be6ebb304bcd0cc366b204b3097
Andrew Clinton <ddrew.clinton@gmail.com>
2015-04-12 15:43:54 -0400

    parent application logic. all controllers inherit from this controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+	include SessionsHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :require_login
+  protected
+
+  def require_login
+    unless current_user
+      redirect_to login_path, :flash => {alert: "You need to login"}
+    end
+    #@current_user = User.find_by(id: session[:user_id])
+    #if @current_user.nil?
+    #	redirect_to login_url, alert: "You need to login"
+    #end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def current_user?(user)
+		user == current_user
+	end
 end
