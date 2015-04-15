76eae9c292575fcf062b5c34b669251aa8eba559
Andrew Clinton <ddrew.clinton@gmail.com>
2015-04-15 11:36:28 -0400

    added validation to game attires

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,10 @@
 class Game < ActiveRecord::Base
 	has_many :users
+
+	email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+	validates :name,						:presence		=>	true
+	validates	:level,						:presence		=>	true
+	validates	:invitee_email,		:presence		=> 	true,
+						:format												=> 	{:with => email_regex}
 end
