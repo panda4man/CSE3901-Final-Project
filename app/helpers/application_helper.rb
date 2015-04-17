8eaea605ff2ff2314cc3e6e5d1fe5e4ee9c46c48
Andrew Clinton <ddrew.clinton@gmail.com>
2015-04-17 01:21:22 -0400

    need to return current user on success apparently

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,11 @@ module ApplicationHelper
 
 	def current_user
 		if session[:user_id]
-			@current_user = User.find(session[:user_id])
+			@current_user = User.find_by_id(session[:user_id])
 			# Actually make sure we got the object
 			# sometimes session exists but user doesn't
 			if !@current_user.nil?
-				return true
+				return @current_user
 			else 
 				return false
 			end
