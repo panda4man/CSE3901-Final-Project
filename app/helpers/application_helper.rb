610c99a1b666dd20d5fe475762ac9a45d7d45690
Andrew Clinton <ddrew.clinton@gmail.com>
2015-04-16 23:42:01 -0400

    added logic to safe guard against deleted users causing the router to crash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,16 @@ module ApplicationHelper
 	end
 
 	def current_user
-		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		if session[:user_id]
+			@current_user = User.find(session[:user_id])
+			# Actually make sure we got the object
+			# sometimes session exists but user doesn't
+			if !@current_user.nil?
+				return true
+			else 
+				return false
+			end
+		end
 	end
 
 	def current_user?(user)
